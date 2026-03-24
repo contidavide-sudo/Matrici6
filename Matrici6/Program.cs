3454db9daf08c7df7c985eba66f0ea9df7c824bb
conti.davide <conti.davide@HP-006>
2026-03-24 10:58:20 +0100

    Continuo

--- Matrici6/Program.cs
+++ Matrici6/Program.cs
@@ -5,22 +5,18 @@
         static int[,] CreaMatrice()
         {
             int[,] mat = new int[10, 10];
-            int cont = 10;
+            int cont1 = 10, cont2 = 10;
             for(int i = 0; i < mat.GetLength(0); i++)
             {
-                for(int j = 0; j < mat.GetLength(1); j++)
+                cont2 = 10;
+
+                for (int j = 0; j < mat.GetLength(1); j++)
                 {
-                    if(i==0 && j == 0)
-                    {
-                        mat[i, j] = mat.GetLength(0) * mat.GetLength(1);
-                    }
-                    else
-                    {
-                        mat[i, j] = mat.GetLength(0) * mat.GetLength(1) - cont;
-                    }
-                        
+                    mat[i, j] = (cont1 * cont2);
+                    cont2 = cont2 - 1;
                 }
-                cont = cont - 1;
+
+                cont1 = cont1 - 1;
             }
             return mat;
         }
@@ -37,11 +33,70 @@
             }
         }
 
+        static int[,] CreaMatEs2()
+        {
+            Random rnd = new Random();
+            int[,] matrice = new int[5, 5];
+
+            for (int i = 0; i < matrice.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrice.GetLength(1); j++)
+                {
+                    matrice[i, j] = rnd.Next(1, 101);
+                }
+            }
+
+            return matrice;
+        }
+
+        static int Contatore(int[,] matrice, int x, int y)
+        {
+            int cont = 0;
+
+            for (int i = 0; i < matrice.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrice.GetLength(1); j++)
+                {
+                    if (matrice[i,j] >= x && matrice[i, j] <= y)
+                    {
+                        cont = cont + 1;
+                    }
+                }
+            }
+
+            return cont;    
+        }
+
+        static float[] Media(int[,] matrice)
+        {
+
+        }
+
         static void Main(string[] args)
         {
-            int[,] matrice= CreaMatrice();
+            int[,] matrice = CreaMatrice();
 
             Stampa(matrice);
+
+            Console.WriteLine();
+
+            int[,] matrice2 = CreaMatEs2();
+
+            Stampa(matrice2);
+
+            int x, y;
+
+            Console.Write("Inserisci x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Inserisci y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int cont = Contatore(matrice2, x, y);
+
+            Console.WriteLine(cont);
         }
     }
 }
